d6f2a510c96499f673dcbf68de53fba050bdd867
CarlosAlberto <cardenas.car@outlook.com>
2024-04-28 21:13:03 -0600

    agrega fila ast

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("Tipo de cuenta: " + tipoDeCuenta);
         System.out.println("Saldo dispoble: " + saldo);
         System.out.println("\n***********************************************");
+        System.out.println("\n***********************************************");
 
         String menu = """
                 -- Escriba el numero de la opcion deseada --
